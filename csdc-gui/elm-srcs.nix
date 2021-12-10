394393b684fbfb74f8ef4d487565748b627dcb7a
guaraqe <mail@guaraqe.gq>
2021-12-11 01:20:12 +0500

    Update elm-nix

--- csdc-gui/elm-srcs.nix
+++ csdc-gui/elm-srcs.nix
@@ -40,6 +40,11 @@
         version = "1.0.2";
       };
 
+      "TSFoster/elm-uuid" = {
+        sha256 = "1a4a7hlp7mynbng9sykicm48zjwkw9w8bsik0zskgwa06qsmjq85";
+        version = "4.1.0";
+      };
+
       "elm/core" = {
         sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
         version = "1.0.5";
@@ -55,9 +60,19 @@
         version = "2.0.0";
       };
 
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
       "elm/bytes" = {
         sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
         version = "1.0.8";
+      };
+
+      "TSFoster/elm-bytes-extra" = {
+        sha256 = "1ip5br0kszhr8kmk19jsqqqhgna89drn3cwb6zn8ncwfrxc8xvs9";
+        version = "1.3.0";
       };
 
       "elm/svg" = {
@@ -65,14 +80,34 @@
         version = "1.0.1";
       };
 
+      "rtfeldman/elm-hex" = {
+        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+        version = "1.0.0";
+      };
+
+      "TSFoster/elm-md5" = {
+        sha256 = "0jgy74sbqqj7gk7a6hh3cv1pnglnpyqbyg8wyf9s9bsipb2zcrrp";
+        version = "2.0.1";
+      };
+
       "elm/parser" = {
         sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
         version = "1.1.0";
       };
 
-      "elm/time" = {
-        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+      "elm/random" = {
+        sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
         version = "1.0.0";
+      };
+
+      "TSFoster/elm-sha1" = {
+        sha256 = "1m2r350gd1xxc7dxzn8dwjv3ir398kigrsspv6agqaasls9n12v6";
+        version = "2.1.1";
+      };
+
+      "danfishgold/base64-bytes" = {
+        sha256 = "1xw4kq7r4zpx484wcnwwkcgzmr4wdxsv3i0j90ynnrjc68ydwgj1";
+        version = "1.1.0";
       };
 
       "elm/virtual-dom" = {
