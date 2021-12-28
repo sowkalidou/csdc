f03ff364f787300a9eabb3476d4b7ae9151a8210
guaraqe <mail@guaraqe.gq>
2021-12-28 14:52:39 +0500

    Check for errors in mail daemon

--- csdc-api/src/CSDC/Daemon/Mail.hs
+++ csdc-api/src/CSDC/Daemon/Mail.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module CSDC.Daemon.Mail
   ( daemon
@@ -8,18 +9,34 @@ module CSDC.Daemon.Mail
 
 import CSDC.Action
 import CSDC.Daemon (Daemon)
-import Control.Monad (forM_)
 
 import qualified CSDC.Daemon as Daemon
 import qualified CSDC.Mail as Mail
 import qualified CSDC.SQL.Mail as SQL.Mail
 
+import Control.Monad (forM_, unless)
+import UnliftIO
+
 -- | A daemon that checks the e-mail queue and tries to send them. If
 -- successful, remove them from the queue.
 daemon :: Daemon (Action user)
 daemon = Daemon.make 5 "Mail" $ do
-  -- XXX: check for errors
+  successesRef <- liftIO $ newIORef (0 :: Int)
+  failuresRef <- liftIO $ newIORef (0 :: Int)
+
   tuples <- runQuery SQL.Mail.select ()
-  forM_ tuples $ \(mailId, mail) -> do
-    runMail (Mail.send mail)
-    runQuery SQL.Mail.delete mailId
+  forM_ tuples $ \(mailId, mail) ->
+    UnliftIO.try (runMail (Mail.send mail)) >>= \case
+      Left (err :: SomeException) -> do
+        liftIO $ print err
+        modifyIORef' failuresRef (+1)
+      Right () -> do
+        runQuery SQL.Mail.delete mailId
+        modifyIORef' successesRef (+1)
+
+  successes <- liftIO $ readIORef successesRef
+  failures <- liftIO $ readIORef failuresRef
+
+  unless (successes + failures == 0) $
+    liftIO $ putStrLn $
+    "Mail: OK - " <> show successes <> ", FAIL - " <> show failures <> "."
